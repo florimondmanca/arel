640a9a24f3d479ffb1dd7cd52e94b3d80df7cd40
florimondmanca <florimond.manca@gmail.com>
2020-08-30 09:12:17 +0200

    Use and pin black 20

--- example/server/app.py
+++ example/server/app.py
@@ -5,5 +5,8 @@ from .events import on_shutdown, on_startup
 from .routes import routes
 
 app = Starlette(
-    debug=settings.DEBUG, routes=routes, on_startup=on_startup, on_shutdown=on_shutdown,
+    debug=settings.DEBUG,
+    routes=routes,
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
 )
